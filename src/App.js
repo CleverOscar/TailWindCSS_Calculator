3d66cfa662d33bea805a80dbaf267ceb9970003b
cleveroscar <Oscarpa9188@gmail.com>
2022-09-13 14:15:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn TailWindCSS
         </a>
       </header>
     </div>
